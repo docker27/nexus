4e5b98e7cf63e30f6825f61ecd468367ee7b4fcd
qianchun <qianchun@huli.com>
2019-08-15 16:43:46 +0800

    init

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
-docker rm -f nexus_lastest
-docker rmi qianchun27/nexus:lastest
-docker build -t qianchun27/nexus:lastest -f Dockerfile .
-docker run --privileged -t -d -p 8084:8084 --name nexus_lastest qianchun27/nexus:lastest /usr/sbin/init;
-docker network connect wind_net nexus_lastest
-docker exec -it nexus_lastest /bin/bash
+docker rm -f nexus
+docker rmi qianchun27/nexus:3.2.0
+docker build -t qianchun27/nexus:3.2.0 -f Dockerfile .
+docker run --privileged -t -d -p 8081:8080 --name nexus qianchun27/nexus:3.2.0 /usr/sbin/init;
+docker network connect wind_net nexus
+docker exec -it nexus /bin/bash
